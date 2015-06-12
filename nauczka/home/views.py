9e95ee4f792dac644d4e4f4ee316d22d614d1972
liscju <piotr.listkiewicz@gmail.com>
2015-06-12 18:28:32 +0200

    Remove duplications in htmls files(navbar,title etc) and moved html files from useless html dir in templates

--- nauczka/home/views.py
+++ nauczka/home/views.py
@@ -5,10 +5,10 @@ from nauczka.home.models import Course
 
 def home_page(request):
     courses = Course.objects.all()
-    return render(request, "html/index.html", {"courses": courses})
+    return render(request, "index.html", {"courses": courses})
 
 def add_online_course(request):
     if request.method == 'POST':
         Course.objects.create(name=request.POST['course_name'],url=request.POST['course_url'])
         return redirect("/")
-    return render(request,"html/add_online_course.html")
+    return render(request,"add_online_course.html")
